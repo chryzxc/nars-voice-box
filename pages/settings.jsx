07f96c9a2ad4e9636aafd8718e79c175c57ae3e4
Christian Rey Villablanca <chriztiann.villablanca@gmail.com>
2024-05-07 01:49:26 +0800

    update

--- pages/settings.jsx
+++ pages/settings.jsx
@@ -1,33 +1,8 @@
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from '@/components/ui/form';
-import { useEffect, useState } from 'react';
-
-import { Button } from '@/components/ui/button';
 import GlobalSettings from '@/page-components/Settings/GlobalSettings';
 import Head from 'next/head';
-import { Input } from '@/components/ui/input';
 import PasswordSettings from '@/page-components/Settings/PasswordSettings';
-import { Textarea } from '@/components/ui/textarea';
-import { fetcher } from '@/lib/fetch';
-import toast from 'react-hot-toast';
 import { useCurrentUser } from '@/lib/user';
-import { useForm } from 'react-hook-form';
 import { userRole } from '@/lib/constants';
-import { z } from 'zod';
-import { zodResolver } from '@hookform/resolvers/zod';
 
 const SettingPage = () => {
   const {
